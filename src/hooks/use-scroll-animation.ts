8eaaab9195a4b590679fb087136db638663e405e
Sanskar <sanskarbhadani.tuff17@gmail.com>
2025-09-19 19:57:36 +0000

    animation should appear every time on scrolling

--- src/hooks/use-scroll-animation.ts
+++ src/hooks/use-scroll-animation.ts
@@ -9,25 +9,21 @@ export const useScrollAnimation = () => {
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        if (entry.isIntersecting) {
-          setInView(true);
-          if (ref.current) {
-            observer.unobserve(ref.current);
-          }
-        }
+        setInView(entry.isIntersecting);
       },
       {
         threshold: 0.1,
       }
     );
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, []);
